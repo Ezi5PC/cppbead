0ca456f352a2328ab6752766f7c03f42b5998e90
zeteny.nagy <zeteny.nagy@tndtech.hu>
2022-12-15 10:16:17 +0100

    minor changes

--- maparray.h
+++ maparray.h
@@ -1,20 +1,23 @@
 #include <iostream>
+#include <map>
 #define MIN(a, b) (a > b ? b : a;)
 
 template <typename U, typename T> class map_array_util{
 	private:
-		std::map<U, T> data;
+		std::map<U, T> * dataptr;
 	public:
-		map_array_util(std::map<U, T> map) : data(map){}
-		bool equals(T[] array, int max){
+		map_array_util(std::map<U, T> map) : data(&map){}
+		bool equals(T array[], int max){
 			for(int i = 0; i < max; i++){
-				if(data.at(i) != array[i]){
+				if(*dataptr->at(i) != array[i]){
 					return false;
 				}
 			}
 			return true;
 		}
-		void copy(T[] array, int to){
-			
+		void copy(T array[], int to){
+			for(int i = 0; i < MIN(MIN(*dataptr->size(), sizeof(array)/sizeof(array[0])), to); i++){
+			    
+			}
 		}
 };
